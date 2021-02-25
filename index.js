9b1e08e0ec9017e79d5732a1b93c23299366172d
nabilacab <74972545+nabilacab@users.noreply.github.com>
2021-02-25 16:34:36 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var h1a = document.getElementById("h1a");
+var btn = document.getElementById("btn");
+
+function apal1() {
+    h1a.innerHTML = "healthy system and good food" ; 
+    btn.innerHTML = "search";
+    
+}
+
+function apal2() {
+    h1a.innerHTML = "نظام الصحة والتغدية الجيدة ";
+    btn.innerHTML ="بحث";
+}
